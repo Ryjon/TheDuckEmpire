5a4bab8316d0842aa6dbec0cefd3dbf179afec16
Smokie <player420@gmail.com>
2019-08-12 19:18:05 -0700

    New commands, 8ball needs fixed and more added to

---
+++ Commands/8ball.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js");
+const fs = require ("fs");
+const ms = require("ms");
+
+module.exports.run = async (bot, message, args) => {
+
+//insert comands here
+if(!args[2]) return message.reply("Thats not a question");
+
+
+let type = args.slice(1);
+if (type = "why") {
+
+let replies = ["yes","No","Impossible to tell","Ask again later"];
+};
+if (type = "who") {
+
+let replies = ["yes","No","Impossible to tell","Ask again later"];
+};
+if (type = "where") {
+
+let replies = ["yes","No","Impossible to tell","Ask again later"];
+};
+if (type = "when") {
+
+let replies = ["yes","No","Impossible to tell","Ask again later"];
+};
+if (type = "how") {
+
+let replies = ["yes","No","Impossible to tell","Ask again later"];
+};
+
+let result = Math.floor((Math.random() * replies.length));
+let question = args.slice(1).join(" ");
+
+let ballembed = new Discord.RichEmbed()
+.setAuthor(message.author.tag)
+.setColor("#29495e")
+.addField("Question", question)
+.addField("Response", replies[result]);
+
+message.channel.send(ballembed);
+
+}
+  module.exports.help = {
+    name: "8ball"
+  }
---
+++ Commands/welcome.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+
+module.exports.run = async (bot, message, args) => {
+
+//insert comands here
+
+
+}
+  module.exports.help = {
+    name: "welcome"
+  }
